82b23e69453c194e9038897ed2b10ee68636682c
Harry C <38871934+HryChg@users.noreply.github.com>
2021-01-15 18:36:19 -0800

    Starter Project

---
+++ Verifyya/Controllers/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Verifyya
+//
+//  Created by Harry Chuang on 1/15/21.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Test Signin
+//        Auth.auth().signIn(withEmail: "123@gmail.com", password: "123456") { [weak self] authResult, error in
+//          guard let strongSelf = self else { return }
+//            if error != nil {
+//                print("Could not sign in user: \(error)")
+//            } else {
+//                print("Successfully Signed In")
+//            }
+//        }
+    }
+
+
+}
+
---
+++ Verifyya/Models/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserModel.swift
+//  Verifyya
+//
+//  Created by Harry Chuang on 1/15/21.
+//
+
+import Foundation
+
+
+struct User {
+    let Name: String
+    // TODO Add more
+}
