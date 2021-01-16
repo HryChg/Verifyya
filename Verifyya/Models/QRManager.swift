8aca52f214eb2f62434ad980bb0ab42c8aa40e0f
hawaiiansurfer4 <sdmurphy.1999@gmail.com>
2021-01-16 13:55:28 -0800

    Starting of QR manager and Wallet Manager

---
+++ Verifyya/Models/QRManager.swift
@@ -0,0 +1,19 @@
+//
+//  QRManager.swift
+//  Verifyya
+//
+//  Created by Sean Murphy on 1/16/21.
+//
+
+import Foundation
+import Firebase
+
+class QRManager {
+    func qrScanner() {
+        
+    }
+    
+    func qrReader() {
+        
+    }
+}
---
+++ Verifyya/Models/WalletManager.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  Verifyya
+//
+//  Created by Sean Murphy on 1/16/21.
+//
+
+import Foundation
+import Firebase
+
+class WalletManager {
+    func updateWallet() {
+        
+    }
+    
+    func didFailToUpdateWallet() {
+        
+    }
+}
