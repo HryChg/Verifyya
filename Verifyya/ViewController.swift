6b9685d19e60f5fc55744d097f5a8efe3ab31c08
Harry C <38871934+HryChg@users.noreply.github.com>
2021-01-15 16:04:36 -0800

    Initial Commit

---
+++ Verifyya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Verifyya
+//
+//  Created by Harry Chuang on 1/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
